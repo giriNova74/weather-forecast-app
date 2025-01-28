f21e068fe2b040b5a99e09cac49dd31546f558ac
GiriPrasad <giriprasad777sdv@gmail.com>
2025-01-28 10:43:06 +0000

    second commit

--- server.py
+++ server.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
-
 def index():
     return render_template('index.html')
 
@@ -16,10 +15,10 @@ def get_weather():
     weather_data = get_current_weather(city)
     return render_template(
         "weather.html",
-          title=weather_data["name"],
-          status=weather_data["weather"][0]["description"].capitalize(),
-          temp= f"{weather_data['main']['temp']:.1f}",
-          feels_like=f"{weather_data['main']['feels_like']:.1f}"
+        title = weather_data["name"],
+        status = weather_data["weather"][0]["description"].capitalize(),
+        temp=f"{weather_data['main']['temp']:.1f}",
+        feels_like = f"{weather_data['main']['feels_like']:.1f}"
     )
 
 if __name__ == "__main__":
